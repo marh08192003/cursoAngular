9d04012922ea8eb011bbdc83f5f5856ee462478f
marh08192003 <riverita470@gmail.com>
2025-08-10 12:50:30 -0500

    signal iputs y creacion de componentes

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from "@angular/core";
+import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
+import { CharacterAddComponent } from "../../components/dragonball/character-app/character-add.component";
 
 interface Character {
   id: number;
@@ -7,18 +9,18 @@ interface Character {
 }
 
 @Component({
-  templateUrl: './dragonball-page.component.html',
+  templateUrl: './dragonball-super-page.component.html',
+  selector: 'dragonball-super',
+  imports: [CharacterListComponent, CharacterAddComponent]
 })
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
 
   name = signal('');
   power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-/*     { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Piccolo', power: 3000 },
-    { id: 4, name: 'Yamcha', power: 500 }, */
+    { id: 2, name: 'Vegeta', power: 8000 },
   ]);
 
   addCharacter() {
@@ -34,14 +36,16 @@ export class DragonballPageComponent {
       : 0;
 
     const newCharacter: Character = {
-      id: maxId + 1,
+      //id: maxId + 1,
+      id: 1000,
       name: this.name(),
       power: this.power(),
     };
 
-    this.characters.update(
+   /*  this.characters.update(
       list => [...list, newCharacter]
-    );
+    ); */
+    console.log({newCharacter});
     this.resetFields();
   }
 
