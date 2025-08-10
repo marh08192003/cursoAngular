6e159b1cd773f3db43a37a8e4363516e5cfb3fcf
marh08192003 <riverita470@gmail.com>
2025-08-10 10:49:46 -0500

    @for iteraciones de elementos

---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from "@angular/core";
+
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  templateUrl:'./dragonball-page.component.html',
+})
+export class DragonballPageComponent{
+
+ characters = signal<Character[]>([
+  {id:1, name: 'Goku', power:9001},
+  {id:2, name: 'Vegeta', power:8000},
+  {id:3, name: 'Piccolo', power:3000},
+ ]);
+
+}
