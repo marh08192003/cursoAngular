651b7ffda6fde004f1091cfdf8a9ad54c5a88528
marh08192003 <riverita470@gmail.com>
2025-08-10 09:20:38 -0500

    Inicio angular

---
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styles: `
+  button{
+    padding:5px;
+    margin: 5px 10px;
+    width: 75px;
+  }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
+})
+export class CounterPageComponent{
+
+  counter = 10;
+  counterSignal = signal(10);
+
+  constructor(){
+    setInterval(() =>{
+      this.counterSignal.update((v) => v + 1);
+      console.log('tick');
+    }, 1 * 1000);
+  }
+
+  increaseBy(value: number){
+    this.counter += value;
+    this.counterSignal.update(current => current + value);
+  }
+
+  resetCounter():void{
+    this.counter = 0;
+    this.counterSignal.set(0);
+  }
+
+
+}
---
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,39 @@
+import { UpperCasePipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, computed, signal } from "@angular/core";
+
+@Component({
+  templateUrl: './hero-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [UpperCasePipe]
+})
+export class HeroPageComponent{
+
+  name = signal('Ironman');
+  age = signal(45);
+
+
+  heroDescription = computed(() =>{
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  })
+
+  capitalizedName = computed(() =>{
+    const capitalized = `${this.name().toUpperCase()}`;
+    return capitalized;
+  })
+
+  changeHero(){
+    this.name.set('Spiderman');
+    this.age.set(22);
+  }
+
+  resetForm(){
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+
+  changeAge(){
+    this.age.set(60);
+  }
+
+}
